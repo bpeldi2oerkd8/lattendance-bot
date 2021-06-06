29d3ec63b0147e13790448849f4efc32a8e818c1
bpeldi2oerkd8 <takahashi208pwla4drb@gmail.com>
2021-06-06 15:37:39 +0900

    返信メッセージの変更

--- scripts/attendance-register.js
+++ scripts/attendance-register.js
@@ -10,27 +10,18 @@ module.exports = robot => {
   robot.respond(/出席[ 　]+(([1-9]|1[0-2])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
     const date = msg.match[1].trim();
     const user_id = msg.message.user.id;
-    const display_name = msg.message.user.slack.profile.display_name;
-    const real_name = msg.message.user.real_name;
-    const user_name = display_name ? display_name : real_name;
-    msg.send(`${user_name}さんが` + date + `に出席します`);
+    msg.send('<@' + user_id + '> さんが' + date + 'に出席します');
   });
 
   robot.respond(/欠席[ 　]+(([1-9]|1[0-2])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
     const date = msg.match[1].trim();
     const user_id = msg.message.user.id;
-    const display_name = msg.message.user.slack.profile.display_name;
-    const real_name = msg.message.user.real_name;
-    const user_name = display_name ? display_name : real_name;
-    msg.send(`${user_name}さんが` + date + `に欠席します`);
+    msg.send('<@' + user_id + '> さんが' + date + 'に欠席します');
   });
 
   robot.respond(/不明[ 　]+(([1-9]|1[0-2])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
     const date = msg.match[1].trim();
     const user_id = msg.message.user.id;
-    const display_name = msg.message.user.slack.profile.display_name;
-    const real_name = msg.message.user.real_name;
-    const user_name = display_name ? display_name : real_name;
-    msg.send(`${user_name}さんの` + date + `の予定は不明です`);
+    msg.send('<@' + user_id + '> さんの' + date + `の予定は不明です`);
   });
 };
