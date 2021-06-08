b3fa7bc6e3b30761355b03bf72f8878a528ff2ed
bpeldi2oerkd8 <takahashi208pwla4drb@gmail.com>
2021-06-09 00:43:07 +0900

    出席登録部分の実装

--- scripts/attendance-register.js
+++ scripts/attendance-register.js
@@ -6,20 +6,50 @@
 //   ボット名 不明     - 不明 を登録
 
 'use strict';
+const api_url = process.env.ATTENDANCE_UPDATE_URL || require('../secret_info/api_url').ATTENDANCE_UPDATE_URL;
 module.exports = robot => {
-  robot.respond(/出席[ 　]+(([1-9]|1[0-2])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
+  robot.respond(/出席[ 　]+((1[0-2]|[1-9])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
+    const roomId = msg.envelope.room;
+    const slackId = msg.message.user.id;
+
     const date = msg.match[1].trim();
-    const user_id = msg.message.user.id;
-    msg.send('<@' + user_id + '> さんが' + date + 'に出席します');
+    const dates = date.split('/');
+    let month = parseInt(dates[0]) >= 1 && parseInt(dates[0]) <= 9 ? '0' + dates[0] : dates[0];
+    let day = parseInt(dates[1]) >= 1 && parseInt(dates[1]) <= 9 ? '0' + dates[1] : dates[1];
+    let dateString = new Date().getFullYear() + '-' + month + '-' + day;
+
+    const update_url = api_url + '/' + roomId + '/users/' + slackId + '/dates/' + dateString;
+    const param = JSON.stringify({
+      availability: 2 //出席
+    });
+    
+    msg.http(update_url)
+    .header('Content-Type', 'application/json')
+    .post(param) ((err, res, body) => {
+      if(err) {
+        msg.send("Error :( " + err);
+        return;
+      }
+
+      const data = JSON.parse(body);
+      if(data.status === 'OK'){
+        msg.send(data.status + '\n' + data.data.scheduleId + '\n' + data.data.userId + '\n' 
+          + data.data.dateId + '\n' + data.data.availability);
+      } 
+      else {
+        msg.send(data.status + '\n' + data.error.messages);
+      }
+    });
+    //msg.send('<@' + slack_id + '> さんが' + date + 'に出席します');
   });
 
-  robot.respond(/欠席[ 　]+(([1-9]|1[0-2])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
+  robot.respond(/欠席[ 　]+((1[0-2]|[1-9])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
     const date = msg.match[1].trim();
     const user_id = msg.message.user.id;
     msg.send('<@' + user_id + '> さんが' + date + 'に欠席します');
   });
 
-  robot.respond(/不明[ 　]+(([1-9]|1[0-2])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
+  robot.respond(/不明[ 　]+((1[0-2]|[1-9])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
     const date = msg.match[1].trim();
     const user_id = msg.message.user.id;
     msg.send('<@' + user_id + '> さんの' + date + `の予定は不明です`);
