3355486bbb4153121f51ded78cf11689716af823
bpeldi2oerkd8 <takahashi208pwla4drb@gmail.com>
2021-06-09 17:10:38 +0900

    出欠更新APIを叩き出欠を更新する機能の完成

--- scripts/attendance-register.js
+++ scripts/attendance-register.js
@@ -13,45 +13,65 @@ module.exports = robot => {
     const slackId = msg.message.user.id;
 
     const date = msg.match[1].trim();
-    const dates = date.split('/');
-    let month = parseInt(dates[0]) >= 1 && parseInt(dates[0]) <= 9 ? '0' + dates[0] : dates[0];
-    let day = parseInt(dates[1]) >= 1 && parseInt(dates[1]) <= 9 ? '0' + dates[1] : dates[1];
-    let dateString = new Date().getFullYear() + '-' + month + '-' + day;
+    let dateString = date2dateString(date);
 
-    const update_url = api_url + '/' + roomId + '/users/' + slackId + '/dates/' + dateString;
-    const param = JSON.stringify({
-      availability: 2 //出席
-    });
-    
-    msg.http(update_url)
-    .header('Content-Type', 'application/json')
-    .post(param) ((err, res, body) => {
-      if(err) {
-        msg.send("Error :( " + err);
-        return;
-      }
-
-      const data = JSON.parse(body);
-      if(data.status === 'OK'){
-        msg.send(data.status + '\n' + data.data.scheduleId + '\n' + data.data.userId + '\n' 
-          + data.data.dateId + '\n' + data.data.availability);
-      } 
-      else {
-        msg.send(data.status + '\n' + data.error.messages);
-      }
-    });
-    //msg.send('<@' + slack_id + '> さんが' + date + 'に出席します');
+    const availability = 2; //出席
+    updateAvailability(msg, roomId, slackId, dateString, availability);
   });
 
   robot.respond(/欠席[ 　]+((1[0-2]|[1-9])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
+    const roomId = msg.envelope.room;
+    const slackId = msg.message.user.id;
+
     const date = msg.match[1].trim();
-    const user_id = msg.message.user.id;
-    msg.send('<@' + user_id + '> さんが' + date + 'に欠席します');
+    let dateString = date2dateString(date);
+
+    const availability = 0; //欠席
+    updateAvailability(msg, roomId, slackId, dateString, availability);
   });
 
   robot.respond(/不明[ 　]+((1[0-2]|[1-9])\/([12][0-9]|3[01]|[1-9]))/i, msg => {
+    const roomId = msg.envelope.room;
+    const slackId = msg.message.user.id;
+
     const date = msg.match[1].trim();
-    const user_id = msg.message.user.id;
-    msg.send('<@' + user_id + '> さんの' + date + `の予定は不明です`);
+    let dateString = date2dateString(date);
+
+    const availability = 1; //不明
+    updateAvailability(msg, roomId, slackId, dateString, availability);
   });
 };
+
+function date2dateString(date) {
+  const dates = date.split('/');
+  let month = parseInt(dates[0]) >= 1 && parseInt(dates[0]) <= 9 ? '0' + dates[0] : dates[0];
+  let day = parseInt(dates[1]) >= 1 && parseInt(dates[1]) <= 9 ? '0' + dates[1] : dates[1];
+  let dateString = new Date().getFullYear() + '-' + month + '-' + day;
+
+  return dateString;
+}
+
+function updateAvailability(msg, roomId, slackId, dateString, availability){
+  const update_url = api_url + '/' + roomId + '/users/' + slackId + '/dates/' + dateString;
+  const param = JSON.stringify({
+    availability: availability
+  });
+    
+  msg.http(update_url)
+  .header('Content-Type', 'application/json')
+  .post(param) ((err, res, body) => {
+    if(err) {
+      msg.send("Error :( " + err);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    if(data.status === 'OK'){
+      msg.send(data.status + '\n' + data.data.scheduleId + '\n' + data.data.userId + '\n' 
+        + data.data.dateId + '\n' + data.data.availability);
+    } 
+    else {
+      msg.send(data.status + '\n' + data.error.messages);
+    }
+  });
+}
